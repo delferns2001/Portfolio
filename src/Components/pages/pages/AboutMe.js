874e08e7c46147fe3bef8b57c805769e2eedd74e
delferns2001 <66013004+delferns2001@users.noreply.github.com>
2021-01-02 22:58:57 +0000

    Updated Nav bar and about me page

--- src/Components/pages/pages/AboutMe.js
+++ src/Components/pages/pages/AboutMe.js
@@ -1,20 +1,33 @@
 import React from "react";
+import "./pages.css";
+import "./img/img1.png";
+import "./img/img2.png";
+import "./img/img3.png";
+import "./img/img4.jpg";
 
 function AboutMe() {
   //  var me = { name: "delton", surname: "fernandes" };
-  var contact = {
-    facebook: "https://www.facebook.com/delton.fernandes.37",
-    instagram: "https://www.instagram.com/fdelton1234/",
-    linkedin: "",
-    github: "",
-  };
 
   return (
     <div>
-      <h1>About Me Page</h1>
-      <a href={contact.facebook}>Facebook</a>
-      {"   "}
-      <a href={contact.instagram}>Instagram</a>
+      <div className="about-me-div">
+        <h1 className="about-me-heading">About Me</h1>
+        <p className="about-me-paragraph">
+          Nullam ac libero nisi. Vestibulum ante ipsum primis in faucibus orci
+          luctus et ultrices posuere cubilia curae; Etiam tempor diam ut elit
+          porta, sit amet tempus nulla rhoncus. Phasellus ac arcu condimentum,
+          tincidunt est a, eleifend erat. Vivamus sagittis suscipit leo in
+          sagittis. Morbi eu massa vel urna pulvinar lobortis. Cras posuere
+          vulputate faucibus. Nulla posuere arcu nec condimentum euismod. Donec
+          pharetra gravida neque at tempus. Etiam imperdiet enim eget nibh
+          sagittis lobortis. In tempor, felis id varius lobortis, sem est
+          venenatis mauris, vitae ornare tellus ligula rutrum nunc. Nunc
+          consequat elementum bibendum. Integer tempor eu enim sed interdum.
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+          per inceptos himenaeos. Ut placerat sem id eros aliquet, a commodo
+          massa gravida. Vestibulum eu tincidunt justo.
+        </p>
+      </div>
     </div>
   );
 }
--- src/Components/pages/pages/Contact.js
+++ src/Components/pages/pages/Contact.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 function contactPage() {
+  var contact = {
+    facebook: "https://www.facebook.com/delton.fernandes.37",
+    instagram: "https://www.instagram.com/fdelton1234/",
+    linkedin: "",
+    github: "",
+  };
+
   return (
     <div>
       <h1>Contact Page</h1>
