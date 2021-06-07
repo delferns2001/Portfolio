d34c3dcc0a3327617894517346483ca2e589f050
delferns2001 <66013004+delferns2001@users.noreply.github.com>
2021-06-07 10:25:28 +0100

    initial dive into custom cursor

--- src/Sections/Projects.js
+++ src/Sections/Projects.js
@@ -1,7 +1,15 @@
 export default function Projects() {
+  const cursor = document.querySelector(`.custom-cursor`);
+  document.addEventListener("mousemove", (e) => {
+    cursor.setAttribute(
+      "style",
+      "top: " + e.pageY + "px; left:" + e.pageX + "px;"
+    );
+  });
+
   return (
     <>
-      <h2>Projects</h2>
+      <h2 className="custom-cursor">Projects</h2>
       <div className="d-flex"></div>
     </>
   );
